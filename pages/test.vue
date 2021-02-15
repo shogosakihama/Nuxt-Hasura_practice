4131570ef7ef80d2266943a4db9665702e88e87b
Shogo Sakihama <goshalways@gmail.com>
2021-02-15 21:28:18 +0900

    参考書でstoreの基礎勉強完了

--- pages/test.vue
+++ pages/test.vue
@@ -3,7 +3,7 @@
     <div>{{ title }}</div>
     <h1>{{ $store.state.message }}</h1>
 
-    <div @click="$store.commit('count')">
+    <div @click="$store.dispatch('doit')">
       <a>clicked: {{ $store.state.counter }}</a>
     </div>
   </div>
--- store/index.js
+++ store/index.js
@@ -9,10 +9,20 @@ const createStore = () => {
       };
     },
     mutations: {
-      count: function(state) {
-        state.counter++;
+      count: function(state, n) {
+        state.counter += n;
+      },
+      say: function(state, msg) {
+        state.message = msg;
       }
     },
+    actions: {
+      doit: function(context) {
+        let n = Math.floor(Math.random() * 10);
+        context.commit('count', n);
+        context.commit('say', 'add' + n + '!');
+      }
+    }
   })
 }
 
